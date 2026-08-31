e02529fd36e9aaf3f17f6f321af8608aec26973c
polluks <polluks@sdf.org>
2026-08-31 09:36:44 +0200

    Rename disk to petsciirobots.dsk (volume PETSCIROB)

--- makedsk.sh
+++ makedsk.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# makedsk.sh - build intro.dsk from the freshly compiled 'intro' binary.
+# makedsk.sh - build petsciirobots.dsk from the freshly compiled 'intro'
+# binary.
 #
-# Creates an 800K ProDOS disk image named PETSCII containing the IIgs
+# Creates an 800K ProDOS disk image named PETSCIROB containing the IIgs
 # executable INTRO. Uses AppleCommander:
 #   https://github.com/AppleCommander/AppleCommander/releases
 # Point AC_JAR at the downloaded AppleCommander jar.
@@ -13,8 +14,8 @@ AC_JAR="${AC_JAR:-AppleCommander-ac-14.0.jar}"
 ls intro >/dev/null 2>&1 || { echo "Run 'make' first to build intro"; exit 1; }
 [ -f "$AC_JAR" ] || { echo "AppleCommander jar not found: $AC_JAR"; exit 1; }
 
-rm -f intro.dsk
-java -jar "$AC_JAR" -pro800 intro.dsk PETSCII
-java -jar "$AC_JAR" -p intro.dsk INTRO EXE 2000 < intro
-echo "Wrote intro.dsk:"
-java -jar "$AC_JAR" -ll intro.dsk
+rm -f petsciirobots.dsk
+java -jar "$AC_JAR" -pro800 petsciirobots.dsk PETSCIROB
+java -jar "$AC_JAR" -p petsciirobots.dsk INTRO EXE 2000 < intro
+echo "Wrote petsciirobots.dsk:"
+java -jar "$AC_JAR" -ll petsciirobots.dsk
